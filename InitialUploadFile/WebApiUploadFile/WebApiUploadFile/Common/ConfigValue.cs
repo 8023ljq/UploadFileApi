180ee8540d73e11a8e6aa88a665f81e17aa55f1f
8023ljq <809395347@qq.com>
2019-10-24 14:10:53 +0800

    添加

---
+++ InitialUploadFile/WebApiUploadFile/WebApiUploadFile/Common/ConfigValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcUploadFile.Common
+{
+    public class ConfigValue
+    {
+        /// <summary>
+        /// 图片上传格式
+        /// </summary>
+        public static string UploadFormat = System.Configuration.ConfigurationManager.AppSettings["UploadFormat"];
+
+        /// <summary>
+        /// 图片上传大小
+        /// </summary>
+        public static string UploadFileSize = System.Configuration.ConfigurationManager.AppSettings["UploadFileSize"];
+
+        /// <summary>
+        /// 图片上传地址
+        /// </summary>
+        public static string ServerImgaes = System.Configuration.ConfigurationManager.AppSettings["ServerImgaes"];
+    }
+}
---
+++ InitialUploadFile/WebApiUploadFile/WebApiUploadFile/Common/WriteLog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MvcUploadFile.Common
+{
+    public class WriteLog
+    {
+        /// <summary>
+        /// Author：Geek Dog  Content：写入错误信息 AddTime：2018-11-16 9:21:42   
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="LogAddress"></param>
+        public static void WriteLogs(Exception ex = null, string LogAddress = "")
+        {
+            //此处理是否存在对应的错误日志文件夹
+            if (ex == null)
+            {
+                StreamWriter fs = new StreamWriter(LogAddress, true);
+                fs.WriteLine(LogAddress);
+                fs.WriteLine();
+                fs.Close();
+            }
+            else
+            {
+                var url = Environment.CurrentDirectory + "\\UploadLog";
+                if (!Directory.Exists(url))
+                {
+                    //创建错误日志文件夹
+                    Directory.CreateDirectory(url);
+                }
+
+
+                //如果日志文件为空，则默认在Debug目录下新建 YYYY-mm-dd_Log.log文件
+                if (LogAddress == "")
+                {
+                    LogAddress = url + '\\' +
+                        DateTime.Now.Year + '-' +
+                        DateTime.Now.Month + '-' +
+                        DateTime.Now.Day + "_Log.txt";
+                }
+                //把异常信息输出到文件，因为异常文件由这几部分组成，这样就不用我们自己复制到文档中了
+                StreamWriter fs = new StreamWriter(LogAddress, true);
+                fs.WriteLine("当前时间：" + DateTime.Now.ToString());
+                fs.WriteLine("异常信息：" + ex.Message);
+                fs.WriteLine("异常对象：" + ex.Source);
+                fs.WriteLine("调用堆栈：\n" + ex.StackTrace.Trim());
+                fs.WriteLine("触发方法：" + ex.TargetSite);
+                fs.WriteLine();
+                fs.Close();
+            }
+
+        }
+    }
+}
