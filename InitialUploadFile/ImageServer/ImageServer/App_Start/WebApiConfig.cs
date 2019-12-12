af4cccc25f1bf9e0f9747fce63dd5f05f834d69b
8023ljq <809395347@qq.com>
2019-12-12 13:42:10 +0800

    修改

--- InitialUploadFile/ImageServer/ImageServer/App_Start/WebApiConfig.cs
+++ InitialUploadFile/ImageServer/ImageServer/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ImageServer
 {
@@ -9,6 +11,16 @@ namespace ImageServer
     {
         public static void Register(HttpConfiguration config)
         {
+            //跨域配置
+            var allowOrigins = ConfigurationManager.AppSettings["cors_allowOrigins"];
+            var allowHeaders = ConfigurationManager.AppSettings["cors_allowHeaders"];
+            var allowMethods = ConfigurationManager.AppSettings["cors_allowMethods"];
+            var globalCors = new EnableCorsAttribute(allowOrigins, allowHeaders, allowMethods)
+            {
+                SupportsCredentials = true,
+            };
+            config.EnableCors(globalCors);
+
             // Web API configuration and services
 
             // Web API routes
